cab6b66b7c475bffc9c8dc389e93097ed48699ca
KOUSHIKJANA007 <koushikj389@gmail.com>
2024-07-21 14:16:51 +0530

    add

--- src/components/News.js
+++ src/components/News.js
@@ -25,11 +25,11 @@ const News = (props) => {
         setArticles(parsedData.articles);
         setTotalResults(parsedData.totalResults);
         setLoading(false);
-        props.setProgress(100);
+        props?.setProgress(100);
     }
     useEffect(() => {
         updateNews();
-        document.title = `${capitalizeFirstLetter(props.catagory)} - NewsMonkey`
+        document.title = `${capitalizeFirstLetter(props?.catagory)} - NewsMonkey`
         //eslint-disable-next-line
     }, [])
 
@@ -78,31 +78,31 @@ const News = (props) => {
     // }
     const fetchMoreData = async () => {
         // this.setState({ page: this.state.page + 1 })
-        const urls = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.catagory}&apiKey=${props.apikey}&page=${page+1}&pageSize=${props.pageSize}`;
+        const urls = `https://newsapi.org/v2/top-headlines?country=${props?.country}&category=${props?.catagory}&apiKey=${props?.apikey}&page=${page+1}&pageSize=${props?.pageSize}`;
         setPage(page + 1)
         let data = await fetch(urls);
         let parsedData = await data.json();
-        setArticles(articles.concat(parsedData.articles))
-        setTotalResults(parsedData.totalResults)
+        setArticles(articles.concat(parsedData?.articles))
+        setTotalResults(parsedData?.totalResults)
     }
 
         return (
             <>
-                <h1 style={{marginTop:"90px"}} className='text-center'>NewsMonkey - Top Headlines from {capitalizeFirstLetter(props.catagory)} </h1>
+                <h1 style={{marginTop:"90px"}} className='text-center'>NewsMonkey - Top Headlines from {capitalizeFirstLetter(props?.catagory)} </h1>
                 {loading && <Spinner />}
                 <InfiniteScroll
-                    dataLength={articles.length}
+                    dataLength={articles?.length}
                     next={fetchMoreData}
-                    hasMore={articles.length !== totalResults}
+                    hasMore={articles?.length !== totalResults}
                     loader={<Spinner />}
                 >
                     <div className="container">
                         <div className="row">
-                            {articles.map((element) => {
-                                return <div className="col-md-4 p-1" key={element.url} >
-                                    <NewsItem title={element.title} description={element.description}
-                                        imageUrl={element.urlToImage ? element.urlToImage : "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_583,w_1036,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1700216831/F_HaJrFXMAA-2RO_qbsqfi"}
-                                        newsUrl={element.url} author={element.author ? element.author : "Unknown"} date={element.publishedAt} source={element.source.name} />
+                            {articles?.map((element) => {
+                                return <div className="col-md-4 p-1" key={element?.url} >
+                                    <NewsItem title={element?.title} description={element?.description}
+                                        imageUrl={element?.urlToImage ? element?.urlToImage : "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_583,w_1036,x_0,y_0/dpr_2.0/c_limit,w_740/fl_lossy,q_auto/v1700216831/F_HaJrFXMAA-2RO_qbsqfi"}
+                                        newsUrl={element?.url} author={element?.author ? element?.author : "Unknown"} date={element?.publishedAt} source={element?.source?.name} />
                                 </div>
                             })}
                         </div>
